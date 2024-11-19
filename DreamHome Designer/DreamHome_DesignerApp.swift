8f5a6d8e7af1f354655e9878289ac8d2f5f9a213
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-11-19 23:07:25 +0300

    Initial Commit

---
+++ DreamHome Designer/DreamHome_DesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamHome_DesignerApp.swift
+//  DreamHome Designer
+//
+//  Created by Роман on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DreamHome_DesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
